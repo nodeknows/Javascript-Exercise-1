8447cda09c311d67f6d159ae2295466051c2e3ce
Asian Himothy <evan.yang23@student.cart.org>
2024-01-24 08:38:30 -0800

    .

--- index.js
+++ index.js
@@ -18,8 +18,14 @@ let kpdec = document.getElementById('decimal')
 let kpequ = document.getElementById('equal')
 let kpadd = document.getElementById('add')
 
+let del = document.getElementById('delete')
+let back = document.getElementById('bckspce')
 let clear = document.getElementById('clear')
 let input = document.getElementById('input')
+
+let eq
+let outsample = document.getElementById('output');
+let paste = document.getElementById('paste');
 
 input.value = localStorage.getItem('input') || ''
 
@@ -27,33 +33,50 @@ function filter(str) {
     str.replace('')
 }
 
-input.oninput = function(){
+input.oninput = function () {
     localStorage.setItem('input', input.value)
 }
 
-clear.onclick = function(){
+clear.onclick = function () {
     location.reload()
 }
 
 function buttonpress(inp) {
-    input.value+=inp;
+    input.value += inp;
 }
 
 //
 
-kp1.onclick = function () {buttonpress(1)}
-kp2.onclick = function () {buttonpress(2)}
-kp3.onclick = function () {buttonpress(3)}
-kpdiv.onclick = function () {buttonpress('/')}
-kp4.onclick = function () {buttonpress(4)}
-kp5.onclick = function () {buttonpress(5)}
-kp6.onclick = function () {buttonpress(6)}
-kpmult.onclick = function () {buttonpress('*')}
-kp7.onclick = function () {buttonpress(7)}
-kp8.onclick = function () {buttonpress(8)}
-kp9.onclick = function () {buttonpress(9)}
-kpmin.onclick = function () {buttonpress('-')}
-kp0.onclick = function () {buttonpress(0)}
-kpdec.onclick = function () {buttonpress('.')}
-//kpequ.onclick = function () {buttonpress(1)}
-kpadd.onclick = function () {buttonpress('+')}
+del.onclick = function () {
+    input.value = ''
+}
+
+back.onclick = function () {
+    input.value = input.value.slice(0, input.value.length-1)
+}
+
+kpequ.onclick = function () {
+    eq = eval(input.value)
+    if (typeof eq !== "number") return;
+    let newsample = outsample.cloneNode(true);
+    newsample.getElementsByTagName('p')[0].innerHTML = input.value
+    newsample.getElementsByTagName('h4')[0].innerHTML = eq
+    newsample.id = ''
+    paste.prepend(newsample)
+}
+
+kp1.onclick = function () { buttonpress(1) }
+kp2.onclick = function () { buttonpress(2) }
+kp3.onclick = function () { buttonpress(3) }
+kpdiv.onclick = function () { buttonpress('/') }
+kp4.onclick = function () { buttonpress(4) }
+kp5.onclick = function () { buttonpress(5) }
+kp6.onclick = function () { buttonpress(6) }
+kpmult.onclick = function () { buttonpress('*') }
+kp7.onclick = function () { buttonpress(7) }
+kp8.onclick = function () { buttonpress(8) }
+kp9.onclick = function () { buttonpress(9) }
+kpmin.onclick = function () { buttonpress('-') }
+kp0.onclick = function () { buttonpress(0) }
+kpdec.onclick = function () { buttonpress('.') }
+kpadd.onclick = function () { buttonpress('+') }
